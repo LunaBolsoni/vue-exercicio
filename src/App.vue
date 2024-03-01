6c07e03ce49a1236ce3eb9627c422da86abb2945
lunabolsoni <bolsoniluna@gmail.com>
2024-03-01 14:06:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+const contador = ref(0)
+function incrementar() {
+  contador.value++;
+}
+function decrementar() {
+  if (contador.value > 0) {
+    contador.value--;
+  }
+}
+function reiniciar() {
+  contador.value = 0
+}
+</script>
+<template>
+  <button @click="incrementar">+</button>
+  <button @click="decrementar">-</button>
+  <button @click="reiniciar">zerar</button>
+  <p>Valor do contador: {{ contador }}</p>
+</template>
+<style scoped>
+button {
+  background-color: black;
+  color: aliceblue;
+  margin: 5pxe;
+  padding: 20px;
+  width: 100px;
+}
+</style>  
